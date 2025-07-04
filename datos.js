6c2fda8265f01ecf263d00c8a2c2504e442eb015
naza2 <102634221+naza2@users.noreply.github.com>
2025-07-04 16:46:30 -0600

    Create datos.js

---
+++ datos.js
@@ -0,0 +1,14 @@
+const excelData = [
+    { valor: 10, mes: 'Enero' },
+    { valor: 20, mes: 'Febrero' },
+    { valor: 30, mes: 'Marzo' },
+    { valor: 25, mes: 'Abril' },
+    { valor: 15, mes: 'Mayo' },
+    { valor: 35, mes: 'Junio' },
+    { valor: 40, mes: 'Julio' },
+    { valor: 50, mes: 'Agosto' },
+    { valor: 45, mes: 'Septiembre' },
+    { valor: 10, mes: 'Octubre' },
+    { valor: 20, mes: 'Noviembre' },
+    { valor: 10, mes: 'Diciembre' }
+];
